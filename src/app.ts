5cc3ca0560920aa87bbf1183564de52f9f90f712
Mert <mert.izgahi@arneca.com>
2023-12-24 19:06:28 +0300

    rate limiter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import config from "config";
 import connect from "./utils/connect";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
 import { _logger } from "./utils/logger";
 import router from "./router";
 import { deserializeUser } from "./middleware/deserializeUser";
@@ -10,6 +11,15 @@ import errorHandler from "./middleware/errorHandler";
 const app = express();
 app.use(express.json());
 app.use(cookieParser(config.get<string>("cookieSecret")));
+app.use(
+  rateLimit({
+    windowMs: config.get<number>("rateLimitWindowMs"), // 15 minutes
+    max: config.get<number>("rateLimitMax"), // limit each IP to 100 requests per windowMs
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+    message: "Too many requests from this IP, please try again later",
+  })
+);
 app.use(deserializeUser);
 const port = config.get<number>("port");
 
